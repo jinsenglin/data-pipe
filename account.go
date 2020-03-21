49b23a1699c4c44d2a0f884b220e09da6be34342
cclin <cclin@chenyuyudeMacBook-Air.local>
2020-03-21 16:52:58 +0800

    update verdor/

--- account.go
+++ account.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+    "github.com/icrowley/fake"
 )
 
-// Golang technique here is encoding.
+// Golang technique here is encoding and marshalling.
 type Account struct {
     AccountID   int64  `spanner:"AccountID" csv:"AccountID"`
+    Name        string `spanner:"Name" csv:"Name"`
+}
+
+func NewAccount(id int64) *Account {
+	return &Account{
+		AccountID:   id,
+		Name:        fake.FullName(),
+	}
 }
--- generate.go
+++ generate.go
@@ -2,14 +2,55 @@ package main
 
 import (
     "log"
+    "sync"
 )
 
 func generate() {
-    // TODO: Implement.
     gcs, err := NewGCSClient()
     if err != nil {
         log.Fatalf("Couldn't connect to Google Cloud Storage: %v", err)
     }
 
-    gcs.list(*bucketName, "")
+    wg := &sync.WaitGroup{}
+
+    wg.Add(1)
+    go generateAccounts(gcs, wg)
+    wg.Add(1)
+    go generateSigners(gcs, wg)
+    wg.Add(1)
+    go generateAlbums(gcs, wg)
+    wg.Add(1)
+    go generateSongs(gcs, wg)
+
+    debugger.Println("Waiting for write to GCS to finish....")
+    wg.Wait()
+}
+
+func generateAccounts(gcs *GCSclient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+    debugger.Printf("Generating %v accounts...", 1000)
+
+    // TODO: Implement.
+}
+func generateSigners(gcs *GCSclient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+    debugger.Printf("Generating %v signers...", 1000)
+
+    // TODO: Implement.
+}
+func generateAlbums(gcs *GCSclient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+    debugger.Printf("Generating %v albums...", 1000)
+
+    // TODO: Implement.
+}
+func generateSongs(gcs *GCSclient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+    debugger.Printf("Generating %v songs...", 1000)
+
+    // TODO: Implement.
 }
