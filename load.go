8f016d9c4dc21a7c9622586cb3a976cee6c44938
cclin <cclin@chenyuyudeMacBook-Air.local>
2020-03-24 23:02:45 +0800

    rev load.go

--- load.go
+++ load.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+    "log"
+)
+
 func load() {
+    gcs, err := NewGCSClient()
+    if err != nil {
+        log.Fatalf("Couldn't connect to Google Cloud Storage: %v", err)
+    }
+
+    files, err := gcs.list(*bucketName, "account-")
+    if err != nil {
+        log.Fatalf("Couldn't list files of prefix account- %v", err)
+    }
+
+    for _, file := range files {
+        debugger.Printf("Loading file %s...", file)
+        gcs.readCSV(*bucketName, file, func(account *Account) {
+            // TODO: Implement.
+        })
+    }
 }
