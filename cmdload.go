27034ef42631b08ad4599e101ec46994622a798e
cclin <cclin@chenyuyudeMacBook-Air.local>
2020-03-26 23:01:33 +0800

    rev cmdload.go

--- cmdload.go
+++ cmdload.go
@@ -27,34 +27,27 @@ func loadAccount(sc *SpannerClient, gcs *GCSclient) {
         log.Fatalf("Couldn't list files of prefix account- %v", err)
     }
 
-    chAccounts := make(chan *Account, *numAccounts)
-    chMutations := make(chan *spanner.Mutation, *numAccounts)
-    chBatchMutations := make(chan []*spanner.Mutation, *numAccounts/100)
+    chAccount := make(chan *Account, *numAccounts)
+    chMutation := make(chan *spanner.Mutation, *numAccounts)
+    chMutations := make(chan []*spanner.Mutation, *numAccounts/100)
 
-    wgReaders := &sync.WaitGroup{}
-    for i, file := range files {
-        debugger.Printf("Starting reader %d for file %s...", i, file)
+    wgLoaders := &sync.WaitGroup{}
+    for i := 0; i < *numLoaders; i++ {
+        debugger.Printf("Starting loader %d", i)
 
-        wgReaders.Add(1)
-        go func() {
-            defer wgReaders.Done()
-            gcs.readCSV(*bucket, file, func(account *Account) {chAccounts <- account})
-        }()
-    }
-
-    wgMutationMakers := &sync.WaitGroup{}
-    // TODO: for i := 0; i < *numAccounts/recordsPerFile; i++ {
-    for i := 0; i < 1; i++ {
-        debugger.Printf("Starting mutation maker %d", i)
-
-        wgMutationMakers.Add(1)
-        go func() {
-            defer wgMutationMakers.Done()
-            for account := range chAccounts {
-                mutation, _ := sc.newMutation("Account", account)
-                chMutations <- mutation
+        wgLoaders.Add(1)
+        go func(in <- chan []*spanner.Mutation) {
+            defer wgLoaders.Done()
+            i := 1
+            for mutations := range in {
+                if err := sc.write(mutations); err != nil {
+                    // TODO: Implement retry.
+                    log.Fatalf("%v", err)
+                }
+                debugger.Printf("Applied batch %d", i)
+                i++
             }
-        }()
+        }(chMutations)
     }
 
     wgBatchMakers := &sync.WaitGroup{}
@@ -63,43 +56,53 @@ func loadAccount(sc *SpannerClient, gcs *GCSclient) {
         debugger.Printf("Starting batch maker %d", i)
 
         wgBatchMakers.Add(1)
-        go func() {
+        go func(in <- chan *spanner.Mutation, out chan <- []*spanner.Mutation) {
             defer wgBatchMakers.Done()
-            for mutation := range chMutations {
-                chBatchMutations <- []*spanner.Mutation{mutation}
+            for mutation := range in {
+                // TODO: Implement batch size.
+                out <- []*spanner.Mutation{mutation}
             }
-        }()
+        }(chMutation, chMutations)
     }
 
-    wgLoaders := &sync.WaitGroup{}
-    for i := 0; i < *numLoaders; i++ {
-        debugger.Printf("Starting loader %d", i)
+    wgMutationMakers := &sync.WaitGroup{}
+    // TODO: for i := 0; i < *numAccounts/recordsPerFile; i++ {
+    for i := 0; i < 1; i++ {
+        debugger.Printf("Starting mutation maker %d", i)
 
-        wgLoaders.Add(1)
-        go func() {
-            defer wgLoaders.Done()
-            i := 1
-            for batchMutation := range chBatchMutations {
-                if err := sc.write(batchMutation); err != nil {
-                    log.Fatalf("%v", err)
-                }
-                debugger.Printf("Applied batch %d", i)
-                i++
+        wgMutationMakers.Add(1)
+        go func(in <- chan *Account, out chan <- *spanner.Mutation) {
+            defer wgMutationMakers.Done()
+            for account := range in {
+                mutation, _ := sc.newMutation("Account", account)
+                // TODO: Implement error handling.
+                out <- mutation
             }
-        }()
+        }(chAccount, chMutation)
+    }
+
+    wgReaders := &sync.WaitGroup{}
+    for i, file := range files {
+        debugger.Printf("Starting reader %d for file %s...", i, file)
+
+        wgReaders.Add(1)
+        go func(out chan <- *Account) {
+            defer wgReaders.Done()
+            gcs.readCSV(*bucket, file, func(account *Account) {out <- account})
+        }(chAccount)
     }
 
     debugger.Println("loadAccount is waiting for readers...")
     wgReaders.Wait()
-    close(chAccounts)
+    close(chAccount)
 
     debugger.Println("loadAccount is waiting for mutation makers...")
     wgMutationMakers.Wait()
-    close(chMutations)
+    close(chMutation)
 
     debugger.Println("loadAccount is waiting for batch makers...")
     wgBatchMakers.Wait()
-    close(chBatchMutations)
+    close(chMutations)
 
     debugger.Println("loadAccount is waiting for loaders...")
     wgLoaders.Wait()
